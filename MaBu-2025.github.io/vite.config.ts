cc53a2a5a49750626ee82cf69c0f90330683fcbe
JJ <jjozef385@gmail.com>
2025-03-09 17:11:33 +0100

    pokus

--- MaBu-2025.github.io/vite.config.ts
+++ MaBu-2025.github.io/vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base: '/MaBu-2025.github.io/MaBu-2025.github.io/'
+   base: '/MaBu-2025.github.io/'
 })
