99e48ea6b0587fff41861149edf9a4711480e9b0
JJ <jjozef385@gmail.com>
2025-03-10 17:08:10 +0100

    change the text style

--- MaBu-2025.github.io/src/components/HelloWorld.vue
+++ MaBu-2025.github.io/src/components/HelloWorld.vue
@@ -5,8 +5,18 @@
 
 <template>
   <div>
-    <h2 class="winter">Wedding is coming</h2>
-    <h3 class="std">Save the date: 16.8.2025</h3>
+    <div class="names">
+      <div class="firstname">BARBORA</div>
+      <div class="surname">Jurašková</div>
+      <div class="surname">a</div>
+      <div class="firstname">MARTIN</div>
+      <div class="surname">Izrael</div>
+    </div>  
+
+    <div class="std">Save the date</div>
+    <div class="date">16.8.2025</div>
+
+    
   </div>
   <!--<div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -38,7 +48,34 @@
   color:bisque;
 }
 .std {
-  color:#888;
-  font-family: 'Papyrus' ;
+  color:#a7a376;
+  font-weight: 100;
+  font-family: "Cardo", serif;
+  font-size: 1.6em;
+}
+.firstname {
+  color:#a7a376;
+  font-family: "Cardo", serif;
+  font-size: medium;
+  letter-spacing: 2px;
+  font-weight: 100;
+  font-style: normal;
+  margin: 0;
+}
+.surname {
+  color:#454411;
+  font-weight: 100;
+  font-family: 'corinthia';  
+  font-size: 3.2em;
+}
+.date {
+  color:#454411;
+  font-weight: 800;
+  font-family: 'corinthia';  
+  font-size: 3.2em;
+}
+.names {
+  margin-top: 100px;
+  margin-bottom: 200px;
 }
 </style>
